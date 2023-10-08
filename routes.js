81471f116fa83da5a16c6ef3efeb167bd3c42043
youatik <y_ouatik@hotmail.com>
2023-10-07 22:43:01 -0400

    PUSH final

--- routes.js
+++ routes.js
@@ -6,10 +6,6 @@ const axios = require('axios'); // Make sure to install this package.
 const PROXY_PORT = 5002
 const PROXY_API_URL = `http://localhost:${PROXY_PORT}`; // replace [PROXY_PORT] with the actual proxy port from your config.
 
-
-
-
-
 module.exports = (app) => {
 
 //route par default
@@ -17,17 +13,17 @@ module.exports = (app) => {
         res.render('home');
     });
 
-// Route pour chercher les 250 permiers documents
+// Route pour chercher les documents
     app.get('/books-catalog', async (req, res) => {
         try {
-            // Request books data from Flask Proxy API
+
             const response = await axios.get(`${PROXY_API_URL}/books`, {
                 headers: {
-                    // Add additional headers here if needed
+
                 }
             });
 
-            // Expecting that response data contains books array directly
+            // Attends la response du data
             const books = response.data;
 
             res.render('book-catalog', { books });
@@ -40,7 +36,7 @@ module.exports = (app) => {
 
 // Route pour le 'Edit Book' dont le formulaire contient les infos à éditer
     app.get('/edit-book/:isbn', async (req, res) => {
-        // This block will handle /edit-book/9780070570641
+        // Ce bloc va handler /edit-book/9780070570641
         handleBookEdit(req, res, req.params.isbn);
     });
 
@@ -58,16 +54,16 @@ module.exports = (app) => {
             // Fetch the book data from the Flask API
             const response = await axios.get(`${PROXY_API_URL}/books/isbn/${isbn}`);
 
-            // Check if the API returned an error (e.g. book not found)
+            // Check si l'API retourne une erreur (e.g. book not found)
             if (response.data.error) {
                 res.status(404).send(response.data.error);
                 return;
             }
 
-            // Get the book data from the API response
+            // Get le book data de l'API response
             const book = response.data;
 
-            //logging for debug
+            //logging pour debug
             console.log(`Looking for book with ISBN: ${isbn}`);
             console.log(book);
 
@@ -79,42 +75,12 @@ module.exports = (app) => {
     }
 
 
-
-    /*
-//fonction de la route 'Edit Book'
-    async function handleBookEdit(req, res, isbn) {
-        try {
-            const db = getDb('A17');
-            const collection = db.collection('Books');
-
-            // Trouver livre par  ISBN (is le ISBN est unique)
-            const book = await collection.findOne({ ean_isbn13: Number(isbn) });
-
-            //logging pour debug
-            console.log(`Looking for book with ISBN: ${isbn}`);
-            const booktest = await collection.findOne({ ean_isbn13: Number(isbn) });
-            console.log(booktest);
-
-            if (!book) {
-                res.status(404).send(`ISBN ${isbn} not found`);
-                return;
-            }
-
-            res.render('edit-book', { book });
-        } catch (error) {
-            console.error(error);
-            res.status(500).json({ error: 'Internal server error' });
-        }
-    }
-*/
-
-
     app.post('/update-book', async (req, res) => {
         try {
             console.log('Enter /update-book route');
             console.log('Request body:', req.body);
 
-            // Process the incoming data to match the Flask API's expected format
+            // Processe le data pour matcher le format Flask API
             const updatedBookData = {
                 ean_isbn13: Number(req.body.isbn),
                 title: req.body.title,
@@ -128,15 +94,15 @@ module.exports = (app) => {
                 length: parseInt(req.body.length, 10),
             };
 
-            // Send the data to Flask API using axios
+            // Envoie le data au Flask API avec axios
             const isbn = req.body.isbn;
             const response = await axios.put(`${PROXY_API_URL}/books/isbn/${isbn}`, updatedBookData);
 
-            // Handle the response from Flask API
+            // Handle la response de Flask API
             if (response.data.error) {
                 res.redirect('/update-book?error=' + encodeURIComponent(response.data.error));
             } else {
-                res.redirect('/books-catalog'); // Redirect to 'book catalog' after updating
+                res.redirect('/books-catalog'); // Redirecte au 'book catalog' apres updating
             }
 
             console.log('Exit /update-book route');
@@ -146,44 +112,6 @@ module.exports = (app) => {
         }
     });
 
-    /*
-// Route pour le 'update book'
-    app.post('/update-book', async (req, res) => {
-        try {
-            //logging pour debug
-            console.log('Enter /update-book route');
-            console.log('Request body:', req.body);
-
-            const db = getDb('A17');
-            const collection = db.collection('Books');
-
-            // données pour le formulaire
-            const updatedBookData = {
-                title: req.body.title,
-                creators: req.body.creators,
-                firstName: req.body.firstName,
-                lastName: req.body.lastName,
-                description: req.body.description,
-                publisher: req.body.publisher,
-                publishDate: req.body.publishDate,
-                price: parseFloat(req.body.price),
-                length: parseInt(req.body.length, 10),
-            };
-
-            // mise à jour sur la base du ISBN
-            const isbn = req.body.isbn;
-            await collection.updateOne({ ean_isbn13: Number(isbn) }, { $set: updatedBookData });
-
-            res.redirect('/books-catalog'); // Rediriger vers le 'book catalog' après le update
-
-            //logging pour debug
-            console.log('Exit /update-book route');
-        } catch (error) {
-            console.error(error);
-            res.status(500).json({ error: 'Internal server error' });
-        }
-    });
-*/
 
 //(VIEW) Route pour la vue 'delete-isbn'
     app.get('/delete-isbn', (req, res) => {
@@ -194,16 +122,16 @@ module.exports = (app) => {
         try {
             const isbn = req.body.isbn;
 
-            // Make a DELETE request to the Flask API
+            // Fait un DELETE request au Flask API
             const response = await axios.delete(`${PROXY_API_URL}/books/isbn/${isbn}`);
 
-            // Check if the Flask API returned an error
+            // Check si le Flask API retourne une error
             if (response.data.error) {
                 res.status(400).send(response.data.error);
                 return;
             }
 
-            res.redirect('/books-catalog');  // Redirect to the 'book catalog' after deletion
+            res.redirect('/books-catalog');  // Redirecte au 'book catalog' apres deletion
 
         } catch (error) {
             console.error(error);
@@ -211,23 +139,6 @@ module.exports = (app) => {
         }
     });
 
-    /*
-    app.post('/delete-book', async (req, res) => {
-        try {
-            const db = getDb('A17');
-            const collection = db.collection('Books');
-
-            const isbn = req.body.isbn;
-            await collection.deleteOne({ ean_isbn13: Number(isbn) });
-
-            res.redirect('/books-catalog');  // Rediriger vers le 'book catalog' après delete
-
-        } catch (error) {
-            console.error(error);
-            res.status(500).json({ error: 'Internal server error' });
-        }
-    });
-*/
 
 // (VIEW) Route pour la vue 'create-book'
     app.get('/create-book', (req, res) => {
@@ -255,14 +166,14 @@ module.exports = (app) => {
                 length: parseInt(req.body.length, 10),
             };
 
-            // Send the data to Flask API
+            // Envoie le data au Flask API
             const response = await axios.post(`${PROXY_API_URL}/books`, newBookData);
 
-            // Assuming the Flask API will return a JSON response indicating success or error
+            // success OU error
             if (response.data.error) {
                 res.redirect('/create-book?error=' + encodeURIComponent(response.data.error));
             } else {
-                res.redirect('/books-catalog'); // Redirect to 'book catalog' after insertion
+                res.redirect('/books-catalog'); // Redirect au 'book catalog' apres insertion
             }
 
             console.log('Exit /create-book route');
@@ -272,53 +183,10 @@ module.exports = (app) => {
         }
     });
 
-    /*
-    app.post('/create-book', async (req, res) => {
-        try {
-            console.log('Enter /create-book route');
-            console.log('Request body:', req.body);
-
-            const db = getDb('A17');
-            const collection = db.collection('Books');
-
-            // Données du formulaire
-            const newBookData = {
-                ean_isbn13: Number(req.body.isbn),
-                title: req.body.title,
-                creators: req.body.creators,
-                firstName: req.body.firstName,
-                lastName: req.body.lastName,
-                description: req.body.description,
-                publisher: req.body.publisher,
-                publishDate: req.body.publishDate,
-                price: parseFloat(req.body.price),
-                length: parseInt(req.body.length, 10),
-            };
-
-            // Vérifier si le ISBN existe déjà
-            const existingBook = await collection.findOne({ ean_isbn13: newBookData.ean_isbn13 });
-
-            if (existingBook) {
-                //  Si le ISBN existe déjà afficher message d'erreur
-                res.redirect('/create-book?error=ISBN already exists');
-            } else {
-                // Insertion dans la BD
-                await collection.insertOne(newBookData);
-
-                res.redirect('/books-catalog'); // Rediriger vers le 'book catalog' après le insert
-            }
-
-            console.log('Exit /create-book route');
-        } catch (error) {
-            console.error(error);
-            res.status(500).json({ error: 'Internal server error' });
-        }
-    }); */
 
 // Route to pour la vue du 'Edit ISBN'
     app.get('/edit-isbn', (req, res) => {
         res.render('edit-isbn');
     });
-
 
 };
